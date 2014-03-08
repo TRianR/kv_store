c42ae4ee59f053e3669c8bbc8b523c2c588ed75a
nileshjagnik <nileshjagnik@gmail.com>
2014-03-08 16:18:31 +0530

    Go formatting done

--- asgn3/main/raftmain.go
+++ asgn3/main/raftmain.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	raft "github.com/nileshjagnik/kv_store/asgn3/lib"
 	"math/rand"
 	"time"
-	"flag"
-	"fmt"
 )
 
 func main() {
-        rand.Seed(time.Now().UTC().UnixNano())
-        id := flag.Int("id", 1, "This is the ID")
-        flag.Parse()
+	rand.Seed(time.Now().UTC().UnixNano())
+	id := flag.Int("id", 1, "This is the ID")
+	flag.Parse()
 	raft.NewServer(*id, "config.txt")
 	select {
-	        case <-time.After(200*time.Second):
-	        fmt.Printf("Time's up!\n")
+	case <-time.After(200 * time.Second):
+		fmt.Printf("Time's up!\n")
 	}
 }
